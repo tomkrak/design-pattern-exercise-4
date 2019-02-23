50664684ff52085ae3a8608b72629291338e223a
tomkrak <tomasz.krakowiak@outlook.com>
2019-02-23 03:00:55 +0100

    Ad 1. Singleton dla klasy AuctionService

--- src/main/java/model/AuctionService.java
+++ src/main/java/model/AuctionService.java
@@ -2,11 +2,31 @@ package model;
 
 public class AuctionService {
     public boolean bid(Auction auction, Customer buyer, double amount) {
-        if (auction.getHighestBid()<amount) {
+        if (auction.getHighestBid() < amount) {
             auction.setHighestBid(amount);
             auction.setBuyer(buyer);
             return true;
         }
         return false;
+    }
+
+    private static AuctionService instance;
+
+    public AuctionService(Auction auction, Customer buyer, double amount) {
+        bid(auction, buyer, amount);
+    }
+
+    public AuctionService() {
+    }
+
+    public static AuctionService getInstance(Auction auction, Customer buyer, double amount) {
+        if (instance == null) {
+            synchronized (AuctionService.class) {
+                if (instance == null) {
+                    instance = new AuctionService(auction, buyer, amount);
+                }
+            }
+        }
+        return instance;
     }
 }
